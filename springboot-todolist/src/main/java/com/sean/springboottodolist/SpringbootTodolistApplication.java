420d9d0e92e42654433f154c78e9e2b2678b63fc
Sean <abbb123p@gmail.com>
2023-09-03 16:58:09 +0800

    init commit

---
+++ springboot-todolist/src/main/java/com/sean/springboottodolist/SpringbootTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.sean.springboottodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTodolistApplication.class, args);
+	}
+
+}
